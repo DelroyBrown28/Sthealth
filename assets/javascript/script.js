47026eb0bedc679f4036e35e5a3f66f54cc36509
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-06-02 22:08:17 +0100

    added animation to get started button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
 
+    $('#get_started_button').click(function () {
 
+
+        $('.main_title').animate({
+            left: '-300px'
+        }, 200, function () {
+            $('.main_title').fadeOut(200, function () {
+                $('.stealth_blurb').animate({
+                    right: '-300px'
+                }, 200, function () {
+                    $('.stealth_blurb').fadeOut(200, function() {
+                        location.href = '#';
+                    });
+                })
+            });
+        })
+
+    })
 
 })
